b1dfbbb74c978423d23e39902e170e8a2a93e586
Burak Tekin <TheBurakTekin@gmail.com>
2022-10-07 05:29:22 +0300

    Second Commit

---
+++ javaRecapDemo/src/javaRecapDemo/enBuyuk.java
@@ -0,0 +1,32 @@
+package javaRecapDemo;
+
+public class enBuyuk {
+
+	public static void main(String[] args) {
+		
+		int sayi1 = 5;
+		int sayi2 = 45;
+		int sayi3 = 13;
+		//s
+		int enBuyuk = sayi1;
+		
+		if(sayi1 > sayi2 && sayi1 > sayi3) {
+			enBuyuk = sayi1;
+			System.out.println("En büyük sayı : " + sayi1);
+		}
+		else if (sayi2 > sayi3 && sayi2 > sayi1) {
+			enBuyuk = sayi2;
+			System.out.println("En büyük sayı : " + sayi2);
+		}
+		else if (sayi3 > sayi2 && sayi3 > sayi1) {
+			enBuyuk = sayi3;
+			System.out.println("En büyük sayı : " + sayi3);
+		}
+		else {
+			System.out.println("Sayilar eşittir");
+		}
+		
+
+	}
+
+}
